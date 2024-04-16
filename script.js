fa1f1c602ad5912eab377acfd7f5e56a2b86380a
Przemek Kurpiel <pkurpiel55@gmail.com>
2024-04-16 16:41:00 +0200

    1.0

--- script.js
+++ script.js
@@ -3,38 +3,24 @@ const addBtn = document.querySelector(".js-addBtn");
 const edit = document.querySelector(".fa-pencil");
 const input = document.querySelector("input");
 
-const li = document.createElement("li");
-
-let listElements = [];
-
-li.classList.add("listElement");
-
-const createDiv = document.createElement("div");
-createDiv.innerHTML = `<i class="fa-solid fa-circle-xmark"></i> 
-      <i class="fa-solid fa-pencil"></i>`;
-createDiv.classList.add("icons");
-
-li.appendChild(createDiv);
-
 const addToList = () => {
   if (input.value === "") {
     return;
   } else {
-    list.appendChild(li);
+    const li = document.createElement("li");
+
+    li.classList.add("listElement");
     li.innerHTML = input.value;
+
+    const createDiv = document.createElement("div");
+    createDiv.innerHTML = `<i class="fa-solid fa-circle-xmark"></i> 
+      <i class="fa-solid fa-pencil"></i>`;
+    createDiv.classList.add("icons");
+
+    li.appendChild(createDiv);
+
+    list.appendChild(li);
     input.value = "";
-
-    const xmarks = document.querySelectorAll(".fa-circle-xmark");
-    const xmarksArr = Array.from(xmarks);
-
-    xmarksArr.forEach((x) => {
-      x.addEventListener(
-        "click",
-        (removeTask = () => {
-          console.log(listElements);
-        })
-      );
-    });
   }
 };
 
