4322cfa51855ad56f068d59bf0af42e27d8053d0
Przemek Kurpiel <pkurpiel55@gmail.com>
2024-04-16 18:15:31 +0200

    deleting added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const list = document.querySelector(".js-list");
 const addBtn = document.querySelector(".js-addBtn");
-const edit = document.querySelector(".fa-pencil");
+const edit = document.querySelector(".pencil");
 const input = document.querySelector("input");
 
 const addToList = () => {
@@ -13,14 +13,38 @@ const addToList = () => {
     li.innerHTML = input.value;
 
     const createDiv = document.createElement("div");
-    createDiv.innerHTML = `<i class="fa-solid fa-circle-xmark"></i> 
-      <i class="fa-solid fa-pencil"></i>`;
     createDiv.classList.add("icons");
 
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "xmark";
+    deleteBtn.innerHTML = `<i class="fa-solid fa-circle-xmark"></i>`;
+
+    const editBtn = document.createElement("button");
+    editBtn.className = "pencil";
+    editBtn.innerHTML = `<i class="fa-solid fa-pencil"></i>`;
+
     li.appendChild(createDiv);
+    li.appendChild(deleteBtn);
+    li.appendChild(editBtn);
 
     list.appendChild(li);
     input.value = "";
+
+    const myList = document.querySelectorAll("li");
+    const myListArr = Array.from(myList);
+
+    for (let i = 0; i < myListArr.length; i++) {
+      function removeTask() {
+        myList[i].remove();
+        return;
+      }
+    }
+
+    const edit = () => {
+      li.innerHTML = input;
+    };
+    deleteBtn.addEventListener("click", removeTask);
+    editBtn.addEventListener("click", edit);
   }
 };
 
